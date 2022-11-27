6770a5aa70186259c14e1940688d867181f67623
Amit Nagmode <Amit.Nagmode1@nhs.net>
2022-11-27 11:55:23 +0000

    using moment-timezone for greeting

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,21 +1,30 @@
-/* globals describe, expect,test
+/* globals describe, expect,test,jest
  */
 const {Greeter} = require('../index.js');
 
 describe('Testing the Greeter', () => {
   test('it should return Hello', () => {
+    Date.now = jest.fn(() => new Date('2022-11-27T12:33:37.0'));
     const geteter = new Greeter();
     const output = 'Hello Amit';
     expect(geteter.greet('Amit')).toEqual(output);
   });
   test('it should trim and greet', () => {
+    Date.now = jest.fn(() => new Date('2022-11-27T13:33:37.0'));
     const geteter = new Greeter();
     const output = 'Hello Amit';
     expect(geteter.greet(' Amit ')).toEqual(output);
   });
   test('it should capitalizes and greet', () => {
+    Date.now = jest.fn(() => new Date('2022-11-27T14:33:37.0'));
     const geteter = new Greeter();
     const output = 'Hello Amit';
+    expect(geteter.greet('amit ')).toEqual(output);
+  });
+  test('it should greet morning', () => {
+    Date.now = jest.fn(() => new Date('2022-11-27T06:33:37.0'));
+    const geteter = new Greeter();
+    const output = 'Good morning Amit';
     expect(geteter.greet('amit ')).toEqual(output);
   });
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
+
 const capitalize = require('capitalize');
+const Moment = require('moment-timezone');
+
 class Greeter {
+  constructor() {
+    this.todaysDt = new Moment();
+    this.todaysDt.tz('Europe/London');
+  }
   greet(name) {
+    if (
+      this.todaysDt.isSameOrAfter(new Moment(this.todaysDt)
+          .set('hour', '06').set('minute', '00')) &&
+        this.todaysDt.isSameOrBefore(new Moment(this.todaysDt)
+            .set('hour', '12').set('minute', '00'))
+    ) {
+      return `Good morning ${capitalize(name.trim())}`;
+    }
     return `Hello ${capitalize(name.trim())}`;
   }
 }
